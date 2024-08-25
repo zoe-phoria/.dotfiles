43adb2ea331f8ec3160f48ca1a7ac9093fe515fe
zoe-phoria <mueller-zoe@pm.me>
2024-08-25 12:52:56 +0200

    remove head statement bc we want to use all monitors don't we

--- .config/polybar/launch.sh
+++ .config/polybar/launch.sh
@@ -3,7 +3,7 @@
 killall -q polybar
 
 echo "---" | tee -a /tmp/polybar.log
-for m in $(xrandr | grep -w 'connected' | awk '{print $1}' | head -n 1); do
+for m in $(xrandr | grep -w 'connected' | awk '{print $1}'); do
     MONITOR=$m polybar --reload | tee -a /tmp/polybar.log & disown
 done
 echo "Bars launched..."

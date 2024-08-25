be4dd601bc703e45aa3171dd1896aabfc29e3acb
zoe-phoria <mueller-zoe@pm.me>
2024-08-25 12:40:16 +0200

    fix --rate match

--- .config/i3/screens.sh
+++ .config/i3/screens.sh
@@ -4,9 +4,9 @@
 last_monitor=
 for monitor in $(xrandr | grep -w 'connected' | awk '{print $1}'); do
     if [[ -z last_monitor ]]; then
-        xrandr --output $monitor --mode $(xrandr | grep $monitor -A 1 | tail -n 1 | awk '{print $1}') --rate $(xrandr | grep $monitor -A 1 | awk '{print $1}')
+        xrandr --output $monitor --mode $(xrandr | grep $monitor -A 1 | tail -n 1 | awk '{print $1}') --rate $(xrandr | grep $monitor -A 1 | tail -n 1 | awk '{print $2}' | sed -e 's/*//g' -e 's/+//g')
     else
-        xrandr --output $monitor --mode $(xrandr | grep $monitor -A 1 | tail -n 1 | awk '{print $1}') --rate $(xrandr | grep $monitor -A 1 | awk '{print $1}') --right-of $last_monitor
+        xrandr --output $monitor --mode $(xrandr | grep $monitor -A 1 | tail -n 1 | awk '{print $1}') --rate $(xrandr | grep $monitor -A 1 | tail -n 1 | awk '{print $2}' | sed -e 's/*//g' -e 's/+//g') --right-of $last_monitor
     fi
     last_monitor=$monitor
 done
